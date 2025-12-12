c977ee30cb60954ea85445cb7551dbac2cb523fd
Balazs P <balazs.p@rankmath.com>
2025-12-12 13:49:33 +0100

    Do not gitignore /vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mockomatic/mockomatic',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '311c8d7d25748b1445037411fdf1a095a10a438f',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mockomatic/mockomatic' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '311c8d7d25748b1445037411fdf1a095a10a438f',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
